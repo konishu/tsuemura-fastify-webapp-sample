2f40fb88107e5800c8288e22eefe18ed09b5d94b
7094vv4 <24241035+7094vv4@users.noreply.github.com>
2025-05-22 15:52:26 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,6 +1,10 @@
 SuiteOf('スモークテスト');
 
-Scenario('example.com にアクセスする',  ({ I }) => {
-  I.amOnPage('https://example.com')
-  I.see('Example Domain')
+Scenario('Webサイトを開きログインする', ({ I }) => {
+  I.amOnPage('https://practical-test-automation-7094vv4.up.railway.app/')
+  I.click('ログインする')
+  I.fillField('ユーザー名' ,'user1')
+  I.fillField('パスワード' ,'super-strong-passphrase')
+  I.click('ログイン')
+  I.see('user1 さん')
 });
