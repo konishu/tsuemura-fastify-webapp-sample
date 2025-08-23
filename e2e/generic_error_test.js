a422f5e8a3493b99ef1213852752f08732821b97
koni0123 <kakikukekonisi2599@gmail.com>
2025-08-23 23:42:07 +0900

    404エラーページの追加とそのテストコードの追加

--- e2e/generic_error_test.js
+++ e2e/generic_error_test.js
@@ -1,7 +1,7 @@
 SuiteOf('一般的なエラーのテスト')
 
 Scenario('ユーザが存在しないURLにアクセスすると、エラーメッセージと商品一覧へのリンクが表示される',({I})=>{
-    I.amOnpage('/undefined')
+    I.amOnPage('/undefined')
     I.see('お探しのページは見つかりませんでした')
     I.click('商品一覧へ戻る')
     I.seeInTitle('商品一覧')
